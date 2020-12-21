ed8b970cc8af22601ba7be9ea728afd71c07a19c
David Boyea <davidmcruz1125@gmail.com>
2020-12-21 13:51:49 -0600

    Adjusts webpack to accept .env file

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,37 @@
 const path = require( 'path' );
-module.exports = {
-  // entry: path.resolve(`${__dirname}/client/src/index.jsx`),
-  entry: './client/src/index.jsx',
-  output: {
-    path: path.join( __dirname, '/client/dist' ),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        exclude: /(node_modules|bower_components)/,
-        test: /\.(js|jsx)?$/,
-        use: {
-          loader: 'babel-loader',
-        }
-      }
+const webpack = require('webpack');
+const dotenv = require('dotenv');
+
+module.exports = () => {
+  // call dotenv and it will return an Object with a parsed key
+  const env = dotenv.config().parsed;
+
+  // reduce it to a nice object, the same as before
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
+
+  return {
+    entry: './client/src/index.jsx',
+    output: {
+      path: path.join( __dirname, '/client/dist' ),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          exclude: /(node_modules|bower_components)/,
+          test: /\.(js|jsx)?$/,
+          use: {
+            loader: 'babel-loader',
+          }
+        },
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin(envKeys)
     ]
-  }
-}
+  };
+};
+

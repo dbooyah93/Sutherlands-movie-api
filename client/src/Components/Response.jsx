45a9a4398462d6e2b583110a06e1192cabd654d8
David Boyea <davidmcruz1125@gmail.com>
2020-12-22 11:20:30 -0600

    Adds ternary to address "horus" unit

--- client/src/Components/Response.jsx
+++ client/src/Components/Response.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Cast from './Cast.jsx';
 
 const Response = ( { title, overview, releaseDate, runTime, top10Cast } ) => {
+  let hours = runTime === '' ? '' : 'hours';
   return (
     <div>
       <h1>Title: { title } </h1>
       <h3>Overview: { overview }</h3>
       <h3>Release Date: { releaseDate }</h3>
-      <h3>Run Time: { runTime } hours</h3>
+      <h3>Run Time: { runTime + ' ' + hours }</h3>
       <h3>Cast: <Cast top10Cast={ top10Cast }/></h3>
     </div>
   )

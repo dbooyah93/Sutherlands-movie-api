d7f7c08c92408c7d0f37944ba877e6109093eb3c
David Boyea <davidmcruz1125@gmail.com>
2020-12-29 11:51:34 -0600

    Adjusts timmer to read as HH:MM

--- client/src/Components/Response.jsx
+++ client/src/Components/Response.jsx
@@ -3,12 +3,20 @@ import Cast from './Cast.jsx';
 
 const Response = ( { title, overview, releaseDate, runTime, top10Cast } ) => {
   let hours = runTime === '' ? '' : 'hours';
+  let hr = '';
+  let min = '';
+  let colon = '';
+  if ( runTime !== '' ) { // so runTime ~~~ 160
+    hr = Math.floor( runTime / 60 );
+    colon = ':';
+    min = runTime % 60;
+  }
   return (
     <div>
       <h1>Title: { title } </h1>
       <h3>Overview: { overview }</h3>
       <h3>Release Date: { releaseDate }</h3>
-      <h3>Run Time: { runTime + ' ' + hours }</h3>
+      <h3>Run Time: { hr + colon + min }</h3>
       <h3>Cast: <Cast top10Cast={ top10Cast }/></h3>
     </div>
   )
